52fd60f897d46c0e9c0b1428eb5eec65012f7d00
嶋崎悠人 <shimazakiyuto@shimazakiyuuninnoiMac.local>
2020-11-07 00:35:13 +0900

    [add] #15 Redux

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import SignUp from './components/pages/SignUp'
 import SignIn from './components/pages/SignIn'
 import Header from './components/templates/Header'
 import Form from './components/pages/Form'
+import slice from './reducks/list/listSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import {useForm} from "react-hook-form"
 
 
-const App = () => {
+
+function App () {
+  const lists = useSelector(state => state.list);
+  const {register, handleSubmit}=useForm()
+
+
+  const dispatch = useDispatch();
+
+    const addList = (date) => {
+      dispatch(slice.actions.addList({ id: lists.length > 0 ? lists.reduce((a, b) => a.id > b.id ? a : b).id + 1 : 1, title: date.newListName }));
+  };
+
+
   return (
     <>
       <Header />
